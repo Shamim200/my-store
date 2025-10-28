909ef34d746ff7b07a21bf41e154cf6bf8db35d8
shamim 200 <s40644812@gmail.com>
2025-10-28 12:55:04 +0600

    ✨ feat(user): implement user authentication controllers
    - create register, login, and logout user controllers
    - implement async error handling for user authentication flow

---
+++ server/src/controllers/user.controller.js
@@ -0,0 +1,8 @@
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { AsyncHandler } from "../utils/Asynchandler.js";
+import { User } from "../models/user.models.js";
+
+export const registerUser = AsyncHandler((req, res) => {});
+export const loginUser = AsyncHandler((req, res) => {});
+export const logoutUser = AsyncHandler((req, res) => {});
