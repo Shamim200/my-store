c0c747b6a3a1fe320aaa89d3ae516feaf068aa29
shamim 200 <s40644812@gmail.com>
2025-10-28 12:55:15 +0600

    ✨ feat(product): implement product controller
    - add basic controller logic for product management
    - implement add, get all, get by id, modify, and remove functionalities

---
+++ server/src/controllers/product.controller.js
@@ -0,0 +1,10 @@
+import { Product } from "../models/product.models.js";
+import { AsyncHandler } from "../utils/Asynchandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+
+export const addProducts = AsyncHandler((req, res) => {});
+export const getAllProducts = AsyncHandler((req, res) => {});
+export const getProductsById = AsyncHandler((req, res) => {});
+export const modifyProducts = AsyncHandler((req, res) => {});
+export const removeProducts = AsyncHandler((req, res) => {});
