85be6a1bc2e1ef685b65842c003f6c264ca2c8ee
shamim 200 <s40644812@gmail.com>
2025-10-28 12:54:35 +0600

    ✨ feat(server): implement user, product, and cart routes - integrate userRouter for handling user-related endpoints - integrate productRouter for managing product-related endpoints - integrate cartRouter for handling shopping cart functionalities

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import userRouter from "./src/routes/user.route.js";
+import productRouter from "./src/routes/product.route.js";
+import cartRouter from "./src/routes/cart.route.js";
 
 const app = express();
 
@@ -13,5 +16,10 @@ app.use(
 app.use(express.urlencoded({ extended: true, limit: "100kb" }));
 app.use(cookieParser());
 app.use(cors());
+
+// define route
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/product", productRouter);
+app.use("/api/v1/cart", cartRouter);
 
 export { app };
